5fe7fa18711260a460ac7781f603a12ef482acdc
Kazuya Ochiai <s23C1033WD@s.chibakoudai.jp>
2025-01-05 18:17:36 +0900

    add a command

--- mypkg/setup.py
+++ mypkg/setup.py
@@ -1,4 +1,4 @@
-"""from setuptools import find_packages, setup
+from setuptools import find_packages, setup
 
 package_name = 'mypkg'
 
@@ -24,33 +24,5 @@ setup(
            # 'listener = mypkg.listener:main',
         ],
     },
-)"""
-
-from setuptools import setup
-import os
-from glob import glob
-
-package_name = 'mypkg'
-
-setup(
-    name=package_name,
-    version='0.0.0',
-    packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='your_name',
-    maintainer_email='your_email@example.com',
-    description='Example package for humidity publisher',
-    license='BSD-3-Clause',
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-            'humidity = mypkg.humidity:main',
-        ],
-    },
 )
+
