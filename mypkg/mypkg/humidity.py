cb9ef7de3fa9d31cdbf1a303a944e7ad8c7ae8e2
Kazuya Ochiai <s23C1033WD@s.chibakoudai.jp>
2025-01-05 18:19:13 +0900

    add a command

--- mypkg/mypkg/humidity.py
+++ mypkg/mypkg/humidity.py
@@ -1,4 +1,4 @@
-"""#!/usr/bin/python3
+#!/usr/bin/python3
 #SPDX-FileCopyrightText: 2025 Kazuya Ochiai
 #SPDX-License-Identigier: BSD-3-Clause
 
@@ -22,35 +22,5 @@ def cb():
 
 def main():
        node.create_timer(1.0, cb)  
-       rclpy.spin(node)   """
+       rclpy.spin(node)   
 
-import rclpy
-from rclpy.node import Node
-from std_msgs.msg import Int16
-import random
-
-class HumidityPublisher(Node):
-    def __init__(self):
-        super().__init__("humidity")
-        self.pub = self.create_publisher(Int16, "tokyo_humidity", 10)
-        self.create_timer(1.0, self.cb)
-
-    def cb(self):        
-        msg = Int16() 
-        msg.data = random.randint(30, 80)     
-        self.pub.publish(msg)
-        print(f"湿度: {msg.data}%")  # テスト用の出力を追加
-       
-def main(args=None):
-    rclpy.init(args=args)
-    node = HumidityPublisher()
-    try:
-        rclpy.spin(node)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        node.destroy_node()
-        rclpy.shutdown()
-
-if __name__ == '__main__':
-    main()
